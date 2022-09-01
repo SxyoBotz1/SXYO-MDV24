d8b7be0888d15ddc36fb8d0db8bc42c1aa468110
Sxyoobotz-MD <112457894+Sxyobotz@users.noreply.github.com>
2022-09-01 18:18:25 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,13 @@
 const qrku = "https://i.ibb.co/MnbqZ0N/qr-Aguz-Familia-07-06-22-1654614928.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
-╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
+╭─「 𝙋𝙪𝙡𝙨𝙖 • 𝙂𝙤𝙥𝙖𝙮 」
+│ • 𝙓𝙡 𝙖𝙭𝙞𝙖𝙩𝙖 [087793236775]
+│ •𝙏𝙚𝙡𝙠𝙤𝙢𝙨𝙚𝙡 [082147562437]
+| • 𝙂𝙊𝙋𝘼𝙔  [08533890541]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281320170984
+│ > Ingin donasi? Wa.me/6287793236775
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
